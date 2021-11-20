6f847ea16580b3c1d960d2021cee6737a98325ce
nktexier <nktexier@gmail.com>
2021-11-20 01:36:47 -0300

    Final
    ultima version

---
+++ Tindividual_final/assets/js/main.js
@@ -0,0 +1,56 @@
+let colors = document.getElementById("todo")
+
+
+function generarLetra(){
+	let letras = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+	let numero = (Math.random()*15).toFixed(0);
+	return letras[numero];
+}
+	
+function colorHEX(){
+	let coolor = "";
+	for(let i=0;i<6;i++){
+		coolor = coolor + generarLetra() ;
+	}
+	return "#" + coolor;
+}
+function changecolor(){
+    let colorido= document.getElementById('todo');
+    colorido.style.background= colorHEX()
+}
+
+
+
+// Initialise Carousel
+const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
+    Dots: false,
+  });
+  
+  // Thumbnails
+  const thumbCarousel = new Carousel(document.querySelector("#thumbCarousel"), {
+    Sync: {
+      target: mainCarousel,
+      friction: 0,
+    },
+    Dots: false,
+    Navigation: false,
+    center: true,
+    slidesPerPage: 1,
+    infinite: false,
+  });
+  
+  // Customize Fancybox
+  Fancybox.bind('[data-fancybox="responsive"]', {
+    Carousel: {
+      on: {
+        change: (that) => {
+          mainCarousel.slideTo(mainCarousel.findPageForSlide(that.page), {
+            friction: 0,
+          });
+        },
+      },
+    },
+  });
+
+ 
+  
